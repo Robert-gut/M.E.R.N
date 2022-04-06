928b294e98609fa5baa65fb4c17a7d36cc815da4
Robert-gut <barnabirobin1@gmail.com>
2022-04-06 14:46:19 +0300

    fixed eroors with req

--- client/src/hooks/http.hooks.js
+++ client/src/hooks/http.hooks.js
@@ -4,16 +4,23 @@ export const useHttp = () => {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
 
-    const request = useCallback(async (url, method = 'GET', body = null, heders = {}) => {
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
         setLoading(true)
         try {
-            const response = await fetch(url, { method, body, heders })
+            if (body) {
+                body = JSON.stringify(body)
+                headers['Content-Type'] = 'application/json'
+            }
+
+            const response = await fetch(url, { method, body, headers })
             const data = await response.json()
 
             if (!response.ok) {
-                throw new Error(data.method || 'Щось пішло не так')
+                throw new Error(data.message || 'Щось пішло не так')
             }
+
             setLoading(false)
+
             return data
         } catch (e) {
             setLoading(false)
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -11,7 +11,7 @@ export const AuthPage = () => {
 
     const registerHandler = async () => {
         try {
-            const data = await request('api/auth/register', 'POST', { ...form })
+            const data = await request('/api/auth/register', 'POST', { ...form })
             console.log('Data', data);
         } catch (e) { }
     }
